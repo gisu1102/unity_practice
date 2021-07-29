00dd3057aa8f4db3a1da115f73bbf1e8676fccc5
gisu1102 <76506573+gisu1102@users.noreply.github.com>
2021-07-29 18:58:28 +0900

    docs:about_study_unity
    unity,c#_basic:BE1-BE8

--- New Unity Project/Assets/Key_Mouse.cs
+++ New Unity Project/Assets/Key_Mouse.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+///키보드 마우스로 이동시키기
+/// </summary>
+
+
 public class Key_Mouse : MonoBehaviour
 {
   
     // Update is called once per frame
-    void Update()
+    void Start()
     {
         //Input //게임 내 입력을 관리하는 클래스 // 
-        
+        /*
         if (Input.anyKeyDown) //아무 입력을 최초로 받을 때 true bool임
             Debug.Log("플레이어가 아무 키를 눌렀습니다");
 
@@ -41,8 +46,45 @@ public class Key_Mouse : MonoBehaviour
         if (Input.GetButtonDown("Jump"))
             Debug.Log("점프!!!");
 
+        //횡 이동  종 이동
+
+        if (Input.GetButton("Horizontal"))
+        {
+            Debug.Log("횡 이동중..." + Input.GetAxisRaw("Horizontal"));
+        }
+        if (Input.GetButton("Vertical"))
+        {
+            Debug.Log("종 이동중..." + Input.GetAxisRaw("Vertical"));
+        }
+        */
 
 
+        //오브젝트는 변수 transform을 항상 가지고 있음 (초기화 완료된 상태)
+        //translate 벡터 값을 현재 값에 더하는 함수
+        //int number = 4; //스칼라 값
+        Vector3 vec = new Vector3(5, 0, 0); //초기화  벡터 값 내가아는 수학벡터 맞음 ㅇㅇ
 
+        transform.Translate(vec);
+
+
+        
     }
+
+    void Update()
+    {
+        Vector3 vec = new Vector3(
+            Input.GetAxis("Horizontal"),    //중간값
+            Input.GetAxisRaw("Vertical"),   //단위가 1
+            0); //초기화  벡터 값 내가아는 수학벡터 맞음 ㅇㅇ
+
+        transform.Translate(vec);
+    }
+
+
+
+
+
+
+
+
 }
---
+++ New Unity Project/Assets/Move_Object.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_Object : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Vector3 target = new Vector3(20, 2, 0);
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //1.MoveTowards (단순 등속 이동)
+        /*
+        transform.position =
+            Vector3.MoveTowards(
+                transform.position,
+                target,
+                1f);      //매개변수 (현재위치,목표위치,속도)로 구성 함수
+        */
+
+        //2.SmoothDamp (미끄러지듯이 감속 이동)
+        Vector3 velo = Vector3.zero;
+        /*
+        transform.position =
+            Vector3.SmoothDamp(transform.position,
+                target,
+                ref velo, //참조 접근 -> 실시간으로 바뀌는 값 적용 가능 ; reference 
+                0.1f
+                );  //마지막 매개변수에 반비례하여 속도증가
+        */
+
+        //3.Lerp (선형 보간 이동), SmoothDamp 보다 감속시간이 김
+        /*
+        transform.position =
+        Vector3.Lerp(
+            transform.position,
+            target,
+            0.1f
+            );  //movetowards 랑 같은 매커니즘
+        */
+
+        //4. SLerp (구면 선형 보간 이동) ,호를 그리며 이동 ;포물선 이동
+        /*
+         transform.position =
+            Vector3.Slerp(
+                transform.position,
+                target,
+                0.01f); 
+        
+        */
+
+        //Time.deltaTime - 이전 프레임 완료까지 걸린 시간 조절 
+        //프레임값이 크면 값이 작고 프레임값이 작으면 값이 큼
+        //컴퓨터 성능에 따라 온라인상에서 프레임이 다르기 때문에 조절 필요
+        //Translate :벡터에 곱하기
+        //transform.Translate(Vec*Time.deltaTime)
+        //Vector함수 : 시간 매개변수에 곱하기
+        //Vector3.Lerp(vec1,vec2,T*Time.deltaTime)
+        
+
+    }
+}
